6be25b551109915fb9462b5c4d0589c07f10d068
yotto3s <yotto.93a@gmail.com>
2023-04-05 22:12:13 +0900

    first commit

---
+++ inits/00_packages.el
@@ -0,0 +1,53 @@
+(use-package solarized-theme
+  :ensure t
+  :config (load-theme 'solarized-dark t))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode)
+  )
+
+(use-package eglot
+  :ensure t
+  :config
+  (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions)
+  (define-key eglot-mode-map (kbd "M-,") 'pop-tag-mark)
+  (add-to-list 'eglot-server-programs
+	           '(julia-mode . ("julia" "-e using LanguageServer, LanguageServer.SymbolServer; runserver()")))
+  (add-hook 'julia-mode-hook 'julia-repl-mode)
+  )
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq company-dabbrev-downcase nil)
+  (setq company-selection-wrap-around t))
+
+(use-package julia-mode
+  :ensure t)
+(use-package julia-repl
+  :ensure t)
+
+(use-package haskell-mode
+  :ensure t
+  :config
+  (require 'haskell-interactive-mode)
+  (require 'haskell-process)
+  (add-hook 'haskell-mode 'interactive-haskell-mode)
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  )
+(use-package elm-mode
+  :ensure t)
+
+(use-package python-mode
+  :ensure t)
