2fc5a3d45de5d42e5bf8c447cf73b52c96921230
yotto <yupon.tysm@gmail.com>
2023-04-10 08:24:57 +0900

    some config

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/org/memo.org"))
  '(package-selected-packages
    '(counsel ivy company-fuzzy ddskk magit which-key use-package solarized-theme python-mode julia-repl julia-mode init-loader helm haskell-mode elm-mode eglot company)))
 (custom-set-faces
